381310056ffa0ca2e011976ce4a8cd1e282252fe
Betsy Vidal <30939075+betsyvies@users.noreply.github.com>
2018-01-26 02:59:35 -0500

    añadiendo evento a los botones

--- js/app.js
+++ js/app.js
@@ -1,69 +1,50 @@
 $(document).ready(() => {
-  var $searchForm = $('#searchForm');
-  var $searchText = $('#searchText');
-  $searchForm.on('submit', (e) => {
-    let word = $searchText.val();
-    console.log(word);
-    cipher(word, 33);
+  $buttonDropdown = $('#button-dropdown');
+  $numberButton = $('#number-button');
+  $containerNumber = $('#container-num');
+  $inputForm = $('#input-form');
+  $sentenceCipher = $('#sentence-cipher');
+  $buttonCipher = $('#button-cipher');
+  $buttonDecipher = $('#button-decipher');
+
+  $buttonCipher.attr('disabled', true);
+  $buttonDecipher.attr('disabled', true);
+
+  $buttonDropdown.on('click', (e) => {
+    $sentenceCipher.html('');
+    $inputForm.val('');
+    $containerNumber.html('');
+
+    for (let i = 1; i <= 100; i++) {
+      $containerNumber.append(`<li class="number">${i}</li>`);
+    }
+    $('#container-num li').on('click', function(e) {  
+      let num = $(this).text();
+      $('#value').html('');    
+      $numberButton.hide();
+      $buttonDropdown.prepend(`<p id="value" class="conteiner-value">${num}</p>`);
+
+      $inputForm
+        .focus()
+        .on('keyup', function(e) {
+          let sentence = $(this).val();
+          if (/^\s+|\s+$/.test(sentence) || /^([0-9])*$/.test(sentence)) {
+            alert('No puedes dejar espacios vacios, ni escribir numeros');
+            $buttonCipher.attr('disabled', true);
+          } else {
+            $buttonCipher
+              .attr('disabled', false)
+              .on('click', (e) => {
+                $sentenceCipher.text(cipher(sentence, num));
+              });
+
+            $buttonDecipher
+              .attr('disabled', false)
+              .on('click', (e) => {
+                $sentenceCipher.text(decipher(sentence, num));
+              });
+          }
+        });
+    });
   });
 });
-
-function findIndex(arr, element) {
-  let position = -1;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (element === arr[i])
-      position = i;
-  }
-  return position;
-};
-
-function cipher(word, n) {
-  var cipherWord = '';
-
-  for (let i = 0; i < word.length; i++) {
-    let temp = word.charAt(i),
-      length = capitalLetter.length,
-      cipherLetter;
-
-    if (findIndex(capitalLetter, temp) !== -1) {
-      cipherLetter = (findIndex(capitalLetter, temp) + n) % length;
-      cipherWord += capitalLetter[cipherLetter];
-    } else if (findIndex(littleletter, temp) !== -1) {
-      cipherLetter = (findIndex(littleletter, temp) + n) % length;
-      cipherWord += littleletter[cipherLetter];
-    } else {
-      cipherLetter = temp;
-      cipherWord += cipherLetter;
-    }
-  }
-  return cipherWord;
-};
-
-// const decipher = (word, n) => {
-//   var decipherWord = '',
-//     length = capitalLetter.length,
-//     n = n % length;
-
-//   for (let i = 0; i < word.length; i++) {
-//     var temp = word.charAt(i),
-//       decipherLetter;
-
-//     if (findIndex(capitalLetter, temp) !== -1) {
-//       decipherLetter = (findIndex(capitalLetter, temp) - n + length) % length;
-
-//       decipherWord += capitalLetter[decipherLetter];
-//     } else if (findIndex(littleletter, temp) !== -1) {
-//       decipherLetter = (findIndex(littleletter, temp) - n + length) % length;
-
-//       decipherWord += littleletter[decipherLetter];
-//     } else {
-//       decipherLetter = temp;
-//       decipherWord += decipherLetter;
-//     }
-//   }
-//   return decipherWord;
-// };
-
-var capitalLetter = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-var littleletter = new Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
