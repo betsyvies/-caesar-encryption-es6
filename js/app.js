9c902c9f359c6ef73303b29bea67218f6885e944
Betsy Vidal <30939075+betsyvies@users.noreply.github.com>
2018-01-26 08:41:57 -0500

    proyecto finalizado

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
   $buttonCipher.attr('disabled', true);
   $buttonDecipher.attr('disabled', true);
 
+  /* Con este evento crearemos los parametros */
   $buttonDropdown.on('click', (e) => {
     $sentenceCipher.html('');
     $inputForm.val('');
@@ -18,12 +19,15 @@ $(document).ready(() => {
     for (let i = 1; i <= 100; i++) {
       $containerNumber.append(`<li class="number">${i}</li>`);
     }
+
+    /* Este evento dará el valor del parametro a la variable p de $buttonDropdown */
     $('#container-num li').on('click', function(e) {  
       let num = $(this).text();
       $('#value').html('');    
       $numberButton.hide();
       $buttonDropdown.prepend(`<p id="value" class="conteiner-value">${num}</p>`);
 
+      /* Este evento recibirá la sentecia y la ejecutará con los botones de cipher y decipher */
       $inputForm
         .focus()
         .on('keyup', function(e) {
@@ -32,12 +36,14 @@ $(document).ready(() => {
             alert('No puedes dejar espacios vacios, ni escribir numeros');
             $buttonCipher.attr('disabled', true);
           } else {
+            /* Este evento ejecutará la función cipher */
             $buttonCipher
               .attr('disabled', false)
               .on('click', (e) => {
                 $sentenceCipher.text(cipher(sentence, num));
               });
-
+              
+            /* Este evento ejecutará la función decipher */
             $buttonDecipher
               .attr('disabled', false)
               .on('click', (e) => {
